bf703c1008ad8a181f6d55219e3fd5d9ee29d724
Merces-dev <giovanitjs013@gmail.com>
2020-10-27 14:42:18 -0300

    header em processo

--- edux/src/components/header/index.jsx
+++ edux/src/components/header/index.jsx
@@ -1,45 +1,21 @@
 import React from 'react';
-import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
-import { useHistory } from 'react-router-dom';
-
+import {Navbar, Nav} from 'react-bootstrap';
+import logo from '../../assets/img/logo-branco.png';
+import '../header/index.css';
 
 const Header = () => {
-    const history = useHistory();
-    const sair = (event) => {
-        localStorage.removeItem('token');
-        history.push('/');
-    }
-
-    return (
-
-        <Navbar collapseOnSelect expand='lg' bg='primary' variant='dark'>
-            <Navbar.Brand href="/">Edux</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="mr-auto">
-                </Nav>
-                <Nav>
-                <Nav.Link href="/">Home</Nav.Link>
-
-                    <Nav.Link href="/">-</Nav.Link>
-                    <Nav.Link href="/">=</Nav.Link>
-                    <Nav.Link href="/">=</Nav.Link>
-                    <Nav.Link href="/">=</Nav.Link>
-                    <Nav.Link href="/">=</Nav.Link>
-
-
-                    <NavDropdown /*title={jwt_decode(token).family_name}*/ id="basic-nav-dropdown">
-                        <NavDropdown.Item href="/perfil">Perfil </NavDropdown.Item >
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item onClick={event => sair(event)}>Sair</NavDropdown.Item >
-                    </NavDropdown>
-
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
-
-    )
-
-};
+  return(
+    <div>
+    <Navbar bg="success" variant="dark">
+    <Navbar.Brand href="#home"><img src={logo} className="nav-logo" alt="Edux"/></Navbar.Brand>
+      <Nav className="mr-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#" >Sobre Nós</Nav.Link>
+      </Nav>
+     
+    </Navbar>
+    </div>
+  )
+}
 
 export default Header;
