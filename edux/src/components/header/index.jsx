eeee96f78c6d5c017f98bababab8502c9f0315b9
Maria Eduarda <eumadu.silva@gmail.com>
2020-10-29 14:10:03 -0300

    Ajustes do header

--- edux/src/components/header/index.jsx
+++ edux/src/components/header/index.jsx
@@ -6,15 +6,22 @@ import '../header/index.css';
 const Header = () => {
   return(
     <div>
-    <Navbar bg="success" variant="dark">
-    <Navbar.Brand href="#home"><img src={logo} className="nav-logo" alt="Edux"/></Navbar.Brand>
+    <Navbar collapseOnSelect  expand="lg" bg="success" variant="dark">
+    <Navbar.Brand href="/"><img src={logo} className="nav-logo" alt="Edux"/></Navbar.Brand>
+    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+    <Navbar.Collapse id="responsive-navbar-nav">
       <Nav className="mr-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="/">Home</Nav.Link>
         <Nav.Link href="#" >Sobre Nós</Nav.Link>
       </Nav>
-     
-    </Navbar>
-    </div>
+      <Nav>
+          <Nav.Link href="/login">Login</Nav.Link>
+          <Nav.Link href="/cadastrar">Cadastrar</Nav.Link>
+      </Nav>
+      </Navbar.Collapse>
+</Navbar>
+    
+   </div>
   )
 }
 
