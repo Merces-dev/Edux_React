019101efd24088654df06116d6435db1f7b0f04d
Merces-dev <giovanitjs013@gmail.com>
2020-11-03 00:02:02 -0300

    Fix - Titulo

--- edux/src/pages/dicas/index.jsx
+++ edux/src/pages/dicas/index.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Container, Row, Col, Card } from 'react-bootstrap';
 import Header from '../../components/header';
 import Footer from '../../components/footer';
-import Titulo from '../../components/tituloDica';
+import Titulo from '../../components/titulo';
 import { url } from '../../utils/constants';
 
 const Dicas = () => {
